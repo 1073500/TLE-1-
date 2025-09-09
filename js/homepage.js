62dae0f69afec59137a35830a7c516e434c9d6f3
lisa-mao <1091575@hr.nl>
2025-09-09 21:45:21 +0200

    homescreen ui

--- js/homepage.js
+++ js/homepage.js
@@ -1,14 +1,40 @@
-let calender
-let thegram
-let poweroff
+// let section1 = document.getElementsByClassName('1')
+//
+//
+//
+// function makeContents() {
+//     let calender = document.createElement('img')
+//     let thegram = document.createElement('a')
+//     let poweroff = document.createElement('img')
+//
+//     calender.src = "images/catwet.png"
+//
+//
+//
+//     let calenderdiv = document.createElement('div')
+//     let thegramdiv = document.createElement('div')
+//     let poweroffdiv = document.createElement('div')
+//
+//
+//     section1.append(calenderdiv, thegramdiv, poweroffdiv)
+//     calenderdiv.appendChild(calender)
+// }
+// makeContents()
 
-init()
-function init() {
-    makeContents()
+function showTime(){
+    let time = new Date()
+    let hours = time.getHours()
+    let minutes = time.getMinutes()
+    let seconds = time.getSeconds()
+
+    let currentTime = hours + ":" + minutes +":" + seconds
+
+    document.getElementsByClassName(
+        'time'
+    )
+        intime.innerhtml(currentTime)
+
+
 }
 
-function makeContents() {
-    calender = document.createElement('image')
-    thegram = document.createElement('a')
-    poweroff = document.createElement('image')
-}
+showTime()
