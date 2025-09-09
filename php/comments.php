51dc2097b768dd63051519bd339f7e827f88e72a
1073500 <1073500@hr.nl>
2025-09-09 12:03:40 +0200

    rename

---
+++ php/comments.php
@@ -0,0 +1,22 @@
+<?php
+//social media pagina 2 comments
+//eerst comment zonder login, daarna met
+
+// 1. connectie met database
+require_once 'includes/database.php';
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>The gram</title>
+</head>
+<body>
+
+</body>
+</html>
